c36c3bde132c1534756f2a30078d464c2817be0c
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2012-08-07 15:28:28 -0400

    Improved the UserResource to pass all the tastytools tests

--- api_app/api/resources.py
+++ api_app/api/resources.py
@@ -4,10 +4,14 @@ from api_app.models import ApiUser
 from datetime import datetime
 from tastypie.authorization import Authorization
 from tastypie.constants import ALL
+from tastypie.exceptions import ImmediateHttpResponse
+from tastypie.http import HttpBadRequest
 from tastytools import fields
 from tastytools.api import Api
 from tastytools.authentication import AuthenticationByMethod
 from tastytools.resources import ModelResource
+
+import json
 
 
 class ApiResource(ModelResource):
@@ -15,6 +19,9 @@ class ApiResource(ModelResource):
     class Meta:
         """ Metadata for the Api resource """
         always_return_data = True
+    def override_urls(self):
+        print "holi"
+        return False
 
 
 class UserResource(ApiResource):
@@ -26,7 +33,9 @@ class UserResource(ApiResource):
     last_name = fields.CharField('last_name', null=True, blank=True,
             help_text="The last name of this user")
     last_login = fields.DateTimeField('last_login', default=datetime.now,
-            help_text="The date of the lat login for this user")
+            help_text="The date of the last login for this user")
+    date_joined = fields.DateTimeField('date_joined', default=datetime.now,
+        help_text="The date when this user joined the app")
 
     class Meta(ApiResource.Meta):
         """ Metadata for the user resource """
@@ -45,6 +54,28 @@ class UserResource(ApiResource):
             'username': ALL,
             'id': ALL,
         }
+
+    def obj_create(self, bundle, request=None, **kwargs):
+        self.is_valid(bundle, request)
+
+        if bundle.errors:
+            self.error_response(bundle.errors, request)
+
+        required_list = [
+            'email',
+            'first_name',
+            'last_name',
+        ]
+
+        for required_field in required_list:
+            if required_field not in bundle.data:
+                response = HttpBadRequest(
+                        json.dumps("missing %s field" % required_field),
+                        content_type=request.META['CONTENT_TYPE'])
+                raise ImmediateHttpResponse(response=response)
+
+        return super(UserResource, self).obj_create(
+            bundle, request, user=request.user, **kwargs)
 
 api = Api(api_name='resources')
 api.register(UserResource())
