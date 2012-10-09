b06a36db2d94a598bc6171794fb16e8cb39bf6ea
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2012-10-09 18:36:32 -0300

    Deleted the api_app app and replaced it with the base app

--- api_app/api/tools.py
+++
@@ -1,9 +0,0 @@
-""" This document defines the tastytools tools for the API """
-from api_app.api.resources import api
-from tastytools.test.resources import ResourceTestData
-
-class UserTestData(ResourceTestData):
-    """ Test Data for the user resource """
-    resource = "user"
-
-api.register_testdata(UserTestData)
--- core/settings.py
+++ core/settings.py
@@ -155,6 +155,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
+    'base',
 )
 
 # A sample logging configuration. The only tangible logging
