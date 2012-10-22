563f3ef0c4ab8a7a7f104d0548b9a661106c1a6e
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2012-10-22 14:43:33 -0300

    Base: Basic password change system

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns
 
 urlpatterns = patterns('',
     (r'^login/$', 'base.views.login'),
+    (r'^password_change/$', 'base.views.password_change'),
 )
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 """ This file contains some generic purpouse views """
 
 from django.contrib.auth.views import login as django_login
+from django.contrib.auth.views import password_change as django_password_change
 from base.forms import AuthenticationForm
 
 
@@ -8,3 +9,9 @@ def login(request):
     """ view that renders the login """
     # If the form has been submitted...
     return django_login(request, authentication_form=AuthenticationForm)
+
+def password_change(request):
+    """ view that renders the login """
+    # If the form has been submitted...
+    return django_password_change(request, post_change_redirect="/",
+            template_name="registration/password_change.html")
