abc0fc1901462c0a86590f57e6b39c3c8e8f4356
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2012-11-15 18:14:54 -0300

    Base.Models: Pep 8 Safe!

--- base/models.py
+++ base/models.py
@@ -34,9 +34,9 @@ class User(DjangoUser, BaseModel):
     """ The representation of a Api user """
 
     # fields
-    token = models.CharField(max_length=30, default="", blank=True,
-        help_text="A token that can be used to verify the user's email",
-    )
+    token = models.CharField(
+        max_length=30, default="", blank=True,
+        help_text="A token that can be used to verify the user's email")
     # Use UserManager to get the create_user method, etc.
     objects = UserManager()
 
@@ -53,7 +53,7 @@ class User(DjangoUser, BaseModel):
     #static methods
     @staticmethod
     def send_email_to(email_list, template_name, subject,
-            template_vars={}, fail_silently=True):
+                      template_vars={}, fail_silently=True):
         """ Sends an email to a list of emails using a given template name """
 
         text_template = get_template("emails/%s.txt" % template_name)
@@ -66,13 +66,13 @@ class User(DjangoUser, BaseModel):
         sender = "%s <%s>" % (settings.EMAIL_SENDER_NAME,
                               settings.SENDER_EMAIL)
         msg = EmailMultiAlternatives(subject, text_content,
-            sender, email_list)
+                                     sender, email_list)
         msg.attach_alternative(html_content, "text/html")
         msg.send(fail_silently=fail_silently)
 
     #private methods
     def _send_email(self, template_name, subject, template_vars={},
-            fail_silently=True):
+                    fail_silently=True):
         """Sends an email to the registered address using a given template
         name
 
@@ -88,22 +88,23 @@ class User(DjangoUser, BaseModel):
 
     #public methods
     def send_email(self, template_name, subject, template_vars=None,
-            fail_silently=True):
+                   fail_silently=True):
         """Sends an email to the registered address using a given template
         name
 
         """
         try:
             t = Thread(target=self._send_email, args=(template_name, subject,
-                template_vars, fail_silently))
+                                                      template_vars,
+                                                      fail_silently))
             t.start()
         except Exception, error:
             if settings.DEBUG:
                 print str(error)
 
     def send_example_email(self):
-        """ Sends an email with the required token so a user can recover his/her
-        password
+        """ Sends an email with the required token so a user can recover
+        his/her password
 
         """
         title = _("Hello")
