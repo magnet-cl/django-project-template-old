d5fd41a0b2cf3634af69c6d07ddf0a950acaf5d8
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2013-06-21 10:03:48 -0400

    Included the auth fixtures in the base app

--- dumpdata.sh
+++ dumpdata.sh
@@ -1,1 +1,2 @@
-python manage.py dumpdata auth users > users/fixtures/initial_data.json --natural --indent=4
+python manage.py dumpdata auth > base/fixtures/initial_data.json --natural --indent=4
+python manage.py dumpdata users > users/fixtures/initial_data.json --natural --indent=4
