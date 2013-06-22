6b3b62758042872b0efda2d437d504b51e6fb691
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2013-06-21 22:45:53 -0400

    Fixed the first and last queries for related managers

--- base/managers.py
+++ base/managers.py
@@ -2,32 +2,6 @@
 
 # django
 from django.db import models
-from django.db.models.query import QuerySet
-
-
-class BaseQuerySet(QuerySet):
-    """
-    This is the base QuerySet, all models should implement it
-    """
-    def last(self):
-        """
-        Returns the last object of a query, returns None if no match is found.
-        """
-        qs = self.reverse() if self.ordered else self.order_by('-pk')
-        try:
-            return qs[0]
-        except IndexError:
-            return None
-
-    def first(self):
-        """
-        Returns the first object of a query, returns None if no match is found.
-        """
-        qs = self if self.ordered else self.order_by('pk')
-        try:
-            return qs[0]
-        except IndexError:
-            return None
 
 
 class BaseManager(models.Manager):
@@ -35,14 +9,24 @@ class BaseManager(models.Manager):
      This is the base manager, all model should implement it
     """
 
-    def get_queryset(self):
-        """Returns a new QuerySet object.  Subclasses can override this method
-        to easily customize the behavior of the Manager.
-        """
-        return BaseQuerySet(self.model, using=self._db)
-
     def last(self):
-        return self.get_query_set().last()
+        qs = self.get_query_set()
+        try:
+            qs = qs.reverse() if qs.ordered else qs.order_by('-pk')
+        except:
+            qs = qs.order_by('-pk')
+        try:
+            return qs[0]
+        except IndexError:
+            return None
 
     def first(self):
-        return self.get_query_set().first()
+        qs = self.get_query_set()
+        try:
+            qs = qs if qs.ordered else qs.order_by('pk')
+        except:
+            qs = qs.order_by('pk')
+        try:
+            return qs[0]
+        except IndexError:
+            return None
