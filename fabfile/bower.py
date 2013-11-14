d9a05cec9cb15d5b792704da0658ef686d0e2949
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2013-11-14 16:17:21 -0300

    fabfile.bower: included messages on tasks to increase verbosity

--- fabfile/bower.py
+++ fabfile/bower.py
@@ -1,9 +1,11 @@
 from fabric.api import task, env, run, cd
+from fabric.colors import green
 
 
 @task
 def install():
     """ Installs bower. """
+    print(green('Installing bower dependencies'))
     run('sudo npm install -g bower')
     update()
 
@@ -11,6 +13,8 @@ def install():
 @task
 def update():
     """ updates bower dependencies """
+    print(green('Updating bower dependencies'))
+
     with cd(env.server_root_dir):
         with cd('base/static/'):
             run('bower install')
