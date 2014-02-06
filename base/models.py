915048b8e450341823d6be3bce7d6f7348f2492b
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2014-02-06 12:41:07 -0300

    New method for base models: get_image_url

--- base/models.py
+++ base/models.py
@@ -7,6 +7,7 @@ All apps should use the BaseModel as parent for all models
 from base.managers import BaseManager
 
 # django
+from django.conf import settings
 from django.db import models
 
 # other
@@ -43,3 +44,16 @@ class BaseModel(models.Model):
             self.__setattr__(kw, kwargs[kw])
 
         self.__class__.objects.filter(pk=self.pk).update(**kwargs)
+
+    def get_image_url(self, field_name="picture", width=None, height=None):
+        """ returns the url of an image stored in the attribute with the name
+        field_name and with the size specified by width and height
+        """
+
+        if width and height:
+            file_path = "{}{}x{}/{}".format(
+                settings.MEDIA_URL, width, height, str(self.picture))
+        else:
+            file_path = "{}{}".format(settings.MEDIA_URL,  str(self.picture))
+
+        return file_path
