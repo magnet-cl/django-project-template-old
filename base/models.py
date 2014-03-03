4ee4c113f24e3851f1d8b030468411e78d596953
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2014-03-03 15:02:41 -0300

    Included thumbnail processing code

--- base/models.py
+++ base/models.py
@@ -12,6 +12,12 @@ from django.db import models
 
 # other
 from django.utils import timezone
+from base.image import generate_thumbnail
+from PIL import Image
+from PIL import ImageFile
+
+# standard library
+import os
 
 
 class BaseModel(models.Model):
@@ -57,3 +63,41 @@ class BaseModel(models.Model):
             file_path = "{}{}".format(settings.MEDIA_URL,  str(self.picture))
 
         return file_path
+
+    def _process_image(self, field_name, image_sizes=None):
+        field = getattr(self, field_name)
+
+        if image_sizes is None:
+            image_sizes = self.image_sizes
+
+        try:
+            file_path = field.file.name
+        except ValueError:
+            pass
+        except IOError:
+            return
+        else:
+            path = file_path.split('.')
+            extension = path[-1]
+
+            if extension != 'jpg':
+
+                ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+                im = Image.open(file_path)
+                path[-1] = 'jpg'
+                final_path = ".".join(path)
+                im = im.convert('RGB')
+                im.save(final_path, 'JPEG')
+                name = self.file_path(os.path.basename(final_path))
+                self.update(**{field_name: name})
+                try:
+                    if im.fp:
+                        im.fp.close()
+                except:
+                    pass
+
+                file_path = final_path
+
+            for size in image_sizes:
+                generate_thumbnail(file_path, size)
