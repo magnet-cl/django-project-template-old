6d19287cbfac9f4abdfcaa8b37a779914d5ee874
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2014-06-02 09:44:15 -0400

    Included the method to extract youtube id

--- base/utils.py
+++ base/utils.py
@@ -2,6 +2,7 @@
 
 # standard library
 import itertools
+import urlparse
 
 
 def grouper(iterable, n):
@@ -9,3 +10,30 @@ def grouper(iterable, n):
     return ([e for e in t if e is not None] for t in itertools.izip_longest(
         *args
     ))
+
+
+def extract_youtube_id(url):
+        """
+        Extracts the youtube id from a youtube url, it supports several
+        formats.
+
+        Based on
+        http://stackoverflow.com/a/7936523/982915
+
+        """
+        if not url:
+            return ''
+
+        query = urlparse.urlparse(url)
+        if query.hostname == 'youtu.be':
+            return query.path[1:]
+        elif query.hostname in ('www.youtube.com', 'youtube.com'):
+            if query.path == '/watch':
+                p = urlparse.parse_qs(query.query)
+                return p['v'][0]
+            if query.path[:7] == '/embed/':
+                return query.path.split('/')[2]
+            if query.path[:3] == '/v/':
+                return query.path.split('/')[2]
+        else:
+            return ''
