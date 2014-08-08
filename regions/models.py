302ca5d81ac5b94d02de1f4ee621de027a2dc785
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2014-08-08 18:26:10 -0400

    New regions app with Chilean Regions and communes

---
+++ regions/models.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from base.models import BaseModel
+from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_noop
+
+# mark for translation the app name
+ugettext_noop("Regions")
+
+
+class Region(BaseModel):
+    name = models.CharField(
+        _('name'), max_length=100, unique=True,
+        help_text=_("The name of the region"),
+    )
+    short_name = models.CharField(
+        _('short name'), max_length=100, null=True, blank=True, unique=True,
+        help_text=_("A shoft name of the region"),
+    )
+
+    class Meta:
+        verbose_name_plural = _("regions")
+        verbose_name = _("region")
+        ordering = ['name']
+
+    def __unicode__(self):
+        return self.name
+
+
+class Commune(BaseModel):
+    # foreign keys
+
+    region = models.ForeignKey(
+        Region,
+    )
+
+    # required fields
+    name = models.CharField(
+        _('name'), max_length=100, unique=True,
+        help_text=_(u'The name of the commune'),
+    )
+
+    class Meta:
+        verbose_name_plural = _(u'communes')
+        verbose_name = _(u'commune')
+        ordering = ['name']
+
+    def __unicode__(self):
+        return self.name
