2f179ce968549778c7d811590739afab43f0b9cf
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2014-08-08 19:48:11 -0400

    Included a list of months in the base app

---
+++ base/definitions.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+""" Definitions commonly used on apps """
+# django
+from django.utils.translation import ugettext_lazy as _
+
+MONTHS = (
+    (0, _(u'Enero')),
+    (1, _(u'Febrero')),
+    (2, _(u'Marzo')),
+    (3, _(u'Abril')),
+    (4, _(u'Mayo')),
+    (5, _(u'Junio')),
+    (6, _(u'Julio')),
+    (7, _(u'Agosto')),
+    (8, _(u'Septiembre')),
+    (9, _(u'Octubre')),
+    (10, _(u'Noviembre')),
+    (11, _(u'Diciembre')),
+)
