5532b1c473ce3fdad055e72be3d55b4b851138b9
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2014-10-22 19:38:03 -0300

    Included translations for the base app

--- base/definitions.py
+++ base/definitions.py
@@ -4,16 +4,16 @@
 from django.utils.translation import ugettext_lazy as _
 
 MONTHS = (
-    (0, _(u'Enero')),
-    (1, _(u'Febrero')),
-    (2, _(u'Marzo')),
-    (3, _(u'Abril')),
-    (4, _(u'Mayo')),
-    (5, _(u'Junio')),
-    (6, _(u'Julio')),
-    (7, _(u'Agosto')),
-    (8, _(u'Septiembre')),
-    (9, _(u'Octubre')),
-    (10, _(u'Noviembre')),
-    (11, _(u'Diciembre')),
+    (0, _(u'January')),
+    (1, _(u'February')),
+    (2, _(u'March')),
+    (3, _(u'April')),
+    (4, _(u'May')),
+    (5, _(u'June')),
+    (6, _(u'July')),
+    (7, _(u'August')),
+    (8, _(u'September')),
+    (9, _(u'October')),
+    (10, _(u'November')),
+    (11, _(u'December')),
 )
