2cee7b3d31fd3446a458e005d91f31993b59250d
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2014-11-17 20:46:48 -0300

    New js functions: app.utils.showLoading and app.utils.hideLoading
    showLoading nos shows a loader icon in the navbar and transforms the
    cursor into a loading cursor
    
    hideLoading reverts the showLoading changes

--- base/static/js/app.js
+++ base/static/js/app.js
@@ -1,19 +1,48 @@
-var App = {
-};
+var App = {};
+var app;
 
-var app = {
-  data: {},
-  messages: {},
-  forms: {
-    datepicker: {
-      format: 'dd/mm/yyyy',
-      language: 'es',
-      autoclose: true
-    },
-    dateTimePicker: {
-      language: 'es'
+(function() {
+  app = {
+    data: {},
+    messages: {},
+    forms: {
+      datepicker: {
+        format: 'dd/mm/yyyy',
+        language: 'es',
+        autoclose: true
+      },
+      dateTimePicker: {
+        language: 'es'
+      }
     }
-  }
-};
+  };
 
-$('.hidden').hide().removeClass('hidden');
+  app.utils = {
+    hideLoading: function() {
+      $('body').removeClass('wait');
+    },
+
+    thousandSeparator: function(x) {
+      x = Math.round(x);
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+    },
+
+    showLoading: function() {
+      $('body').addClass('wait');
+    },
+
+    highlight: function($el) {
+      $el.addClass('highlight');
+      setTimeout(function() {
+        $el.toggleClass('dim highlight');
+      }, 15);
+      setTimeout(function() {$el.removeClass('dim');}, 1010);
+    }
+  };
+
+  $('.hidden').hide().removeClass('hidden');
+
+  $('.alert').each(function() {
+    app.utils.highlight($(this));
+  });
+}());
