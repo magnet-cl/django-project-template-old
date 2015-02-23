7f2bde5fe2c22584b3a6bb3842d73bd1c408150a
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2015-02-23 20:32:12 -0300

    Update the way in which media files are served

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,7 @@
 
 from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 
@@ -12,13 +13,4 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     (r'^accounts/', include('users.urls')),
     url(r'^$', 'base.views.index', name='home'),
-)
-
-
-if settings.DEBUG:
-    urlpatterns += patterns(
-        '',
-        (r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:],
-         'django.views.static.serve', {
-             'document_root': settings.MEDIA_ROOT})
-    )
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
