da858fc64bc32872dd4e8a61827f064caf252f85
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2016-02-08 08:38:15 -0300

    Add view that returns JSON

--- base/views.py
+++ base/views.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """ This file contains some generic purpouse views """
 
+# standard library
+import json
+
+# django
 from django.contrib.auth.decorators import login_required
+from django.core.serializers.json import DjangoJSONEncoder
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
@@ -31,3 +37,11 @@ def page_not_found_view(request):
 def error_view(request):
     return render_to_response('exceptions/500.jade', {},
                               context_instance=RequestContext(request))
+
+
+def json_view(request, response=None):
+    if response is None:
+        response = {}
+
+    return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder),
+                        content_type="application/json")
