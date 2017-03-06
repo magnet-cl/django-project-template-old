4e2b4f9b563c1bd51b91b140f2f2cb97d5e3296f
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2017-03-06 08:51:32 -0300

    Restore translation script

--- translate.sh
+++ translate.sh
@@ -4,16 +4,56 @@ function print_green(){
     echo -e "\e[32m$1\e[39m"
 }
 
+function print_red(){
+    echo -e "\e[31m$1\e[39m"
+}
+
 function print_blue(){
     echo -e "\e[34m$1\e[39m"
 }
 
-function translate {
-    if $COMPILE ; then
-        django-admin.py compilemessages
-    else
+function makemessages {
+    if [ -d $1 ]; then 
+        cd $1
+
+        mkdir -p locale
+
         django-admin.py makemessages -l es -e jade,html,txt,py
-        django-admin.py makemessages -d djangojs -l es -i "base/static/bower_components" -i "node_modules" -e jade,js
+        diff=$(git diff --numstat locale/es/LC_MESSAGES/django.po)
+        lineCount=(${diff// / })
+
+        if [ $lineCount ] ; then
+            if [ $lineCount == 1 ] ; then
+                git checkout locale/es/LC_MESSAGES/django.po
+            fi
+        fi
+
+        django-admin.py makemessages -d djangojs -l es -i "static/bower_components" -e jade,js
+
+        if [ -f locale/es/LC_MESSAGES/djangojs.po ]; then
+            diff=$(git diff --numstat locale/es/LC_MESSAGES/djangojs.po)
+            lineCount=(${diff// / })
+            if [ $lineCount ] ; then
+                if [ $lineCount == 1 ] ; then
+                    git checkout locale/es/LC_MESSAGES/djangojs.po
+                fi
+            fi
+        fi
+
+        cd ..
+    else
+        print_red "folder $1 does not exist"
+    fi
+}
+
+function translate {
+    print_green "translating $1"
+    if $COMPILE ; then
+        cd $1
+        django-admin.py compilemessages
+        cd ..
+    else
+        makemessages $1
     fi
 }
 
@@ -33,20 +73,15 @@ do
      esac
 done
 
+rm -r project/static/CACHE/*
+
 if [ $1 ] && [ $1 != '-c' ] ; then 
-    print_blue "Translate only on app $1"
-    cd $1
-    mkdir -p locale
-    translate
-    cd ..
+    print_blue "Only on app $1"
+    translate $1
 elif [ $2 ] && [ $2 != '-c' ] ; then 
-    print_blue "Translate only on app $2"
-    cd $2
-    mkdir -p locale
-    translate
-    cd ..
+    print_blue "Only on app $2"
+    translate $2
 else
-    mkdir -p locale
-    print_blue "Translate all apps"
-    translate
+    translate "base"
+    translate "users"
 fi
